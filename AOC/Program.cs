e70f08c8380c7ecfad14eb8049593b65c188ff9b
Jonathan Rupp <jrupp@rightpoint.com>
2018-12-06 23:22:07 -0600

    Day 7 - #6 for the day for the second star.

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -32,7 +32,10 @@ namespace AoC
             //new Year2018.Day5.Faster().Run();
 
             //new Year2018.Day6.Part1().Run();
-            new Year2018.Day6.Part2().Run();
+            //new Year2018.Day6.Part2().Run();
+
+            //new Year2018.Day7.Part1().Run();
+            new Year2018.Day7.Part2().Run();
 
             if (Debugger.IsAttached)
             {

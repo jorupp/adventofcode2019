f90c0b0513f0e9d74237e19bdd4a227c4a57cfd5
Jonathan Rupp <jrupp@rightpoint.com>
2018-12-17 00:11:15 -0600

    Day 17 - 2x low-30s on the global leaderboard

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -67,7 +67,6 @@ namespace AoC
             //new Year2018.Day16.Part2().Run();
 
             new Year2018.Day17.Part1().Run();
-            new Year2018.Day17.Part2().Run();
 
             if (Debugger.IsAttached)
             {

262ca7dfb616d0175120d6b6d2d216e46e80450e
Jonathan Rupp <jrupp@rightpoint.com>
2018-12-21 23:50:56 -0600

    Day 22 - no points for part 1, many points for A* in part 2

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -80,7 +80,7 @@ namespace AoC
             //new Year2018.Day21.Part1().Run();
             //new Year2018.Day21.Part2().Run();
 
-            new Year2018.Day22.Part1().Run();
+            //new Year2018.Day22.Part1().Run();
             new Year2018.Day22.Part2().Run();
 
             if (Debugger.IsAttached)

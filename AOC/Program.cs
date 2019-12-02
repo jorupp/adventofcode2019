ba93d2d7266e88ad76f47ce762b0863653d6c920
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-02 11:20:53 -0600

    Prep for 2019

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -12,86 +12,86 @@ namespace AoC
 
             //new Year2016.Day11.Part1().Run();
 
-            //new Year2018.Day0.Part1().Run();
-            //new Year2018.Day0.Part2().Run();
+            //new Year2019.Day0.Part1().Run();
+            //new Year2019.Day0.Part2().Run();
 
-            //new Year2018.Day1.Part1().Run();
-            //new Year2018.Day1.Part2().Run();
+            //new Year2019.Day1.Part1().Run();
+            //new Year2019.Day1.Part2().Run();
 
-            //new Year2018.Day2.Part1().Run();
-            //new Year2018.Day2.Part2().Run();
+            //new Year2019.Day2.Part1().Run();
+            //new Year2019.Day2.Part2().Run();
 
-            //new Year2018.Day3.Part1().Run();
-            //new Year2018.Day3.Part2().Run();
+            //new Year2019.Day3.Part1().Run();
+            //new Year2019.Day3.Part2().Run();
 
-            //new Year2018.Day4.Part1().Run();
-            //new Year2018.Day4.Part2().Run();
+            //new Year2019.Day4.Part1().Run();
+            //new Year2019.Day4.Part2().Run();
 
-            //new Year2018.Day5.Part1().Run();
-            //new Year2018.Day5.Part2().Run();
-            //new Year2018.Day5.Faster().Run();
+            //new Year2019.Day5.Part1().Run();
+            //new Year2019.Day5.Part2().Run();
+            //new Year2019.Day5.Faster().Run();
 
-            //new Year2018.Day6.Part1().Run();
-            //new Year2018.Day6.Part2().Run();
+            //new Year2019.Day6.Part1().Run();
+            //new Year2019.Day6.Part2().Run();
 
-            //new Year2018.Day7.Part1().Run();
-            //new Year2018.Day7.Part2().Run();
+            //new Year2019.Day7.Part1().Run();
+            //new Year2019.Day7.Part2().Run();
 
-            //new Year2018.Day8.Part1().Run();
-            //new Year2018.Day8.Part2().Run();
+            //new Year2019.Day8.Part1().Run();
+            //new Year2019.Day8.Part2().Run();
 
-            //new Year2018.Day9.Part2().Run();
-            //new Year2018.Day9.Part1().Run();
+            //new Year2019.Day9.Part2().Run();
+            //new Year2019.Day9.Part1().Run();
 
-            //new Year2018.Day10.Part1().Run();
+            //new Year2019.Day10.Part1().Run();
 
-            //new Year2018.Day11.Part1().Run();
-            //new Year2018.Day11.Part2().Run();
-            //new Year2018.Day11.Part3().Run();
+            //new Year2019.Day11.Part1().Run();
+            //new Year2019.Day11.Part2().Run();
+            //new Year2019.Day11.Part3().Run();
 
-            //new Year2018.Day12.Part1().Run();
-            //new Year2018.Day12.Part2().Run();
+            //new Year2019.Day12.Part1().Run();
+            //new Year2019.Day12.Part2().Run();
 
-            //new Year2018.Day13.Part1().Run();
-            //new Year2018.Day13.Part2().Run();
+            //new Year2019.Day13.Part1().Run();
+            //new Year2019.Day13.Part2().Run();
 
-            //new Year2018.Day14.Part1().Run();
-            //new Year2018.Day14.Part2().Run();
+            //new Year2019.Day14.Part1().Run();
+            //new Year2019.Day14.Part2().Run();
 
-            //new Year2018.Day15.Part1().Run();
-            //new Year2018.Day15.Part2().Run();
+            //new Year2019.Day15.Part1().Run();
+            //new Year2019.Day15.Part2().Run();
 
-            //new Year2018.Day16.Part1().Run();
-            //new Year2018.Day16.Part2a().Run();
-            //new Year2018.Day16.Part2b().Run();
-            //new Year2018.Day16.Part2().Run();
+            //new Year2019.Day16.Part1().Run();
+            //new Year2019.Day16.Part2a().Run();
+            //new Year2019.Day16.Part2b().Run();
+            //new Year2019.Day16.Part2().Run();
 
-            //new Year2018.Day17.Part1().Run();
+            //new Year2019.Day17.Part1().Run();
 
-            //new Year2018.Day18.Part1().Run();
-            //new Year2018.Day18.Part2().Run();
+            //new Year2019.Day18.Part1().Run();
+            //new Year2019.Day18.Part2().Run();
 
-            //new Year2018.Day19.Part1().Run();
-            //new Year2018.Day19.Part2().Run();
-            //new Year2018.Day19.Part2b().Run();
+            //new Year2019.Day19.Part1().Run();
+            //new Year2019.Day19.Part2().Run();
+            //new Year2019.Day19.Part2b().Run();
 
-            //new Year2018.Day20.Part1().Run();
+            //new Year2019.Day20.Part1().Run();
 
-            //new Year2018.Day21.Part1().Run();
-            //new Year2018.Day21.Part2().Run();
+            //new Year2019.Day21.Part1().Run();
+            //new Year2019.Day21.Part2().Run();
 
-            //new Year2018.Day22.Part1().Run();
-            //new Year2018.Day22.Part2().Run();
+            //new Year2019.Day22.Part1().Run();
+            //new Year2019.Day22.Part2().Run();
 
-            //new Year2018.Day23.Part1().Run();
-            //new Year2018.Day23.Part2().Run();
-            //new Year2018.Day23.Part2b().Run();
-            new Year2018.Day23.Part2c().Run();
+            //new Year2019.Day23.Part1().Run();
+            //new Year2019.Day23.Part2().Run();
+            //new Year2019.Day23.Part2b().Run();
+            //new Year2019.Day23.Part2c().Run();
 
-            //new Year2018.Day24.Part1().Run();
-            //new Year2018.Day24.Part2().Run();
+            //new Year2019.Day24.Part1().Run();
+            //new Year2019.Day24.Part2().Run();
 
-            //new Year2018.Day25.Part1().Run();
+            //new Year2019.Day25.Part1().Run();
 
             if (Debugger.IsAttached)
             {

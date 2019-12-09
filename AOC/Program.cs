cb4eb438b23c83f82c2e1537105d9a8064a5294e
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-08 23:26:46 -0600

    Day 9

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -38,12 +38,12 @@ namespace AoC
             //new Year2019.Day7.Part1().Run();
             //new Year2019.Day7.Part2().Run();
 
-            new Year2019.Day8.Part1().Run();
-            new Year2019.Day8.Part2().Run();
-            new Year2019.Day8.Part2b().Run();
+            //new Year2019.Day8.Part1().Run();
+            //new Year2019.Day8.Part2().Run();
+            //new Year2019.Day8.Part2b().Run();
 
+            new Year2019.Day9.Part1().Run();
             //new Year2019.Day9.Part2().Run();
-            //new Year2019.Day9.Part1().Run();
 
             //new Year2019.Day10.Part1().Run();
 

0fe85b6b2cd7c3e3efa2674dad8f3ba975268fd9
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-12 22:00:56 -0600

    working setup for d12-p2

--- AOC/Year2019/Day12/Part2.cs
+++ AOC/Year2019/Day12/Part2.cs
@@ -1,67 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AoC.Year2019.Day12
 {
     public class Part2 : BasePart
     {
-        protected void RunScenario(string title, int[][] position, int stepCount)
+        protected void RunScenario(string title, int[][] position)
         {
             RunScenario(title, () =>
             {
-                var moons = position.Select(i => new Moon(i, new[] { 0, 0, 0 })).ToList();
+                var parts = Enumerable.Range(0, 3)
+                    .Select(ii => SimulateDimension(position.Select(i => i[ii]).ToArray())).ToList();
 
-                Console.WriteLine($"After {0} steps");
-                foreach (var moon in moons)
-                {
-                    Console.WriteLine(moon.ToString());
-                }
-                Console.WriteLine($"Energy: {moons.Sum(i => i.Energy)}");
-                Console.WriteLine();
-                for (var step = 1; step <= stepCount; step++)
-                {
-                    moons[0].ApplyGravity(moons[1]);
-                    moons[0].ApplyGravity(moons[2]);
-                    moons[0].ApplyGravity(moons[3]);
-                    moons[1].ApplyGravity(moons[2]);
-                    moons[1].ApplyGravity(moons[3]);
-                    moons[2].ApplyGravity(moons[3]);
-
-                    moons[0].ApplyVelocity();
-                    moons[1].ApplyVelocity();
-                    moons[2].ApplyVelocity();
-                    moons[3].ApplyVelocity();
-
-                    //for (var i = 0; i < moons.Count; i++)
-                    //{
-                    //    for (var j = i + 1; j < moons.Count; j++)
-                    //    {
-                    //        moons[i].ApplyGravity(moons[j]);
-                    //    }
-                    //}
-                    //for (var i = 0; i < moons.Count; i++)
-                    //{
-                    //    moons[i].ApplyVelocity();
-                    //}
-
-                }
-
-                Console.WriteLine($"After {stepCount} steps");
-                foreach (var moon in moons)
-                {
-                    Console.WriteLine(moon.ToString());
-                }
-                Console.WriteLine($"Energy: {moons.Sum(i => i.Energy)}");
-                Console.WriteLine();
+                var lcm = parts.Aggregate((a, i) => determineLCM(a, i));
+                Console.WriteLine($"Repeats in {lcm} steps");
             });
+        }
+
+        private static long SimulateDimension(int[] position)
+        {
+            var moons = position.Select(i => new Moon(i, 0)).ToList();
+            var seen = new Dictionary<State, long>();
+
+            seen[new State(moons)] = 0;
+
+            long step = 1;
+            for (; ; step++)
+            {
+                moons[0].ApplyGravity(moons[1]);
+                moons[0].ApplyGravity(moons[2]);
+                moons[0].ApplyGravity(moons[3]);
+                moons[1].ApplyGravity(moons[2]);
+                moons[1].ApplyGravity(moons[3]);
+                moons[2].ApplyGravity(moons[3]);
+
+                moons[0].ApplyVelocity();
+                moons[1].ApplyVelocity();
+                moons[2].ApplyVelocity();
+                moons[3].ApplyVelocity();
+
+                var state = new State(moons);
+                if (seen.ContainsKey(state))
+                {
+                    break;
+                }
+
+                seen[state] = step;
+            }
+
+            Console.WriteLine($"After {step} steps, found a loop back to {seen[new State(moons)]}");
+            Console.WriteLine();
+            return step;
+        }
+
+        // https://stackoverflow.com/a/13569863/12502
+        public static long determineLCM(long a, long b)
+        {
+            if (a < b) return determineLCM(b, a);
+
+            for (long i = 1; i < b; i++)
+            {
+                if ((a * i) % b == 0)
+                {
+                    return i * a;
+                }
+            }
+            return a * b;
+        }
+
+
+        private class State
+        {
+            private List<int> values;
+
+            public State(ICollection<Moon> moons)
+            {
+                this.values = moons.SelectMany(i => new [] { i.p, i.v }).ToList();
+
+            }
+
+            public override bool Equals(object obj)
+            {
+                var s = (State) obj;
+                return this.values.Zip(s.values, (i1, i2) => i1 == i2).All(i => i);
+            }
+
+            public override int GetHashCode()
+            {
+                return this.values.Aggregate((a, i) => a ^ i.GetHashCode());
+            }
         }
 
         private class Moon
         {
-            private int[] p;
-            private int[] v;
+            public int p;
+            public int v;
 
-            public Moon(int[] p, int[] v)
+            public Moon(int p, int v)
             {
                 this.p = p;
                 this.v = v;
@@ -69,35 +105,30 @@ namespace AoC.Year2019.Day12
 
             public void ApplyGravity(Moon other)
             {
-                for (var i = 0; i < this.p.Length; i++)
+                if (other.p > this.p)
                 {
-                    if (other.p[i] > this.p[i])
-                    {
-                        other.v[i]--;
-                        this.v[i]++;
-                    }
-                    else if (other.p[i] < this.p[i])
-                    {
-                        other.v[i]++;
-                        this.v[i]--;
-                    }
+                    other.v--;
+                    this.v++;
+                }
+                else if (other.p < this.p)
+                {
+                    other.v++;
+                    this.v--;
                 }
             }
 
             public void ApplyVelocity()
             {
-                for (var i = 0; i < this.p.Length; i++)
-                {
-                    this.p[i] += this.v[i];
-                }
+                    this.p += this.v;
             }
 
-            public override string ToString()
-            {
-                return $"pos=<x={p[0]}, y={p[1]}, z={p[2]}>, vel=<x={v[0]},y={v[1]},z={v[2]}>";
-            }
 
-            public int Energy => p.Sum(Math.Abs) * v.Sum(Math.Abs);
+            //public override string ToString()
+            //{
+            //    return $"pos=<x={p[0]}, y={p[1]}, z={p[2]}>, vel=<x={v[0]},y={v[1]},z={v[2]}>";
+            //}
+
+            //public int Energy => p.Sum(Math.Abs) * v.Sum(Math.Abs);
         }
 
         public override void Run()
@@ -108,14 +139,14 @@ namespace AoC.Year2019.Day12
                 new [] { 2, -10, -7},
                 new [] { 4, -8, 8},
                 new [] { 3, 5, -1},
-            }, 4000000);
+            });
             RunScenario("initial", new[]
             {
                 new [] { -8, -10, 0},
                 new [] { 5, 5, 10},
                 new [] { 2, -7, 3},
                 new [] { 9, -8, -3},
-            }, 4000000);
+            });
             //return;
             RunScenario("real", new[]
             {
@@ -123,7 +154,7 @@ namespace AoC.Year2019.Day12
                 new [] { -14, 9, -4},
                 new [] { -4, -6, 7 },
                 new [] { 6, -9, -11},
-            }, 4000000);
+            });
 
         }
     }

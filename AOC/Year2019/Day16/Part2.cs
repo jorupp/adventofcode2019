7acfcb4644ad8957909e576fdcc43a8d07063507
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-16 00:37:16 -0600

    hacky hack hack

--- AOC/Year2019/Day16/Part2.cs
+++ AOC/Year2019/Day16/Part2.cs
@@ -17,72 +17,72 @@ namespace AoC.Year2019.Day16
                 numbers = Enumerable.Range(0, 10000).SelectMany(i => numbers).ToArray();
                 var offset = int.Parse(string.Join("", numbers.Take(7)));
                 var startPoint = offset - numPhases;
-                //numbers = numbers.Skip(startPoint).ToArray();
-                //Console.WriteLine($"Will use ix {ix}, starting at {startPoint}, output will be from {ix - startPoint} of {numbers.Length}");
+                numbers = numbers.Skip(startPoint).ToArray();
+                Console.WriteLine($"Will use offset {offset}, starting at {startPoint}, output will be from {offset - startPoint} of {numbers.Length}");
 
-                var cache = new Dictionary<(int, int), int>();
+                //var cache = new Dictionary<(int, int), int>();
 
-                string GetCalclation(int iteration, int ix)
-                {
-                    if (iteration == 0)
-                    {
-                        return $"Number[{ix}]";
-                    }
+                //string GetCalclation(int iteration, int ix)
+                //{
+                //    if (iteration == 0)
+                //    {
+                //        return $"Number[{ix}]";
+                //    }
 
-                    var x = new List<string>();
-                    for (var jx = startPoint; jx < numbers.Length; jx++)
-                    {
-                        switch (((jx + 1) / (ix + 1)) % 4)
-                        {
-                            case 1:
-                                x.Add("+" + GetCalclation(iteration - 1, jx));
-                                break;
-                            case 3:
-                                x.Add("-" + GetCalclation(iteration - 1, jx));
-                                break;
-                        }
-                    }
+                //    var x = new List<string>();
+                //    for (var jx = startPoint; jx < numbers.Length; jx++)
+                //    {
+                //        switch (((jx + 1) / (ix + 1)) % 4)
+                //        {
+                //            case 1:
+                //                x.Add("+" + GetCalclation(iteration - 1, jx));
+                //                break;
+                //            case 3:
+                //                x.Add("-" + GetCalclation(iteration - 1, jx));
+                //                break;
+                //        }
+                //    }
 
-                    return $"(0{string.Join("", x)})";
-                }
+                //    return $"(0{string.Join("", x)})";
+                //}
 
-                Console.WriteLine(GetCalclation(1, offset));
-                return;
+                //Console.WriteLine(GetCalclation(1, offset));
+                //return;
 
 
-                int GetValue(int iteration, int ix)
-                {
-                    if (iteration == 0)
-                    {
-                        return numbers[ix];
-                    }
-                    var key = (iteration, offset: ix);
-                    if (cache.TryGetValue(key, out var v))
-                    {
-                        return v;
-                    }
+                //int GetValue(int iteration, int ix)
+                //{
+                //    if (iteration == 0)
+                //    {
+                //        return numbers[ix];
+                //    }
+                //    var key = (iteration, offset: ix);
+                //    if (cache.TryGetValue(key, out var v))
+                //    {
+                //        return v;
+                //    }
 
-                    //Console.WriteLine($"Calculating {iteration} @ {ix}");
+                //    //Console.WriteLine($"Calculating {iteration} @ {ix}");
 
-                    int r = 0;
-                    for (var jx = startPoint; jx < numbers.Length; jx++)
-                    {
-                        switch (((jx + 1) / (ix + 1)) % 4)
-                        {
-                            case 1:
-                                r += GetValue(iteration - 1, jx);
-                                break;
-                            case 3:
-                                r -= GetValue(iteration - 1, jx);
-                                break;
-                        }
-                    }
+                //    int r = 0;
+                //    for (var jx = startPoint; jx < numbers.Length; jx++)
+                //    {
+                //        switch (((jx + 1) / (ix + 1)) % 4)
+                //        {
+                //            case 1:
+                //                r += GetValue(iteration - 1, jx);
+                //                break;
+                //            case 3:
+                //                r -= GetValue(iteration - 1, jx);
+                //                break;
+                //        }
+                //    }
 
-                    v = r % 10;
-                    cache[key] = v;
+                //    v = r % 10;
+                //    cache[key] = v;
 
-                    return v;
-                }
+                //    return v;
+                //}
 
                 //var basePattern = new long[] { 0, 1, 0, -1 };
 
@@ -102,53 +102,75 @@ namespace AoC.Year2019.Day16
                 //var patterns = Enumerable.Range(0, numbers.Count).Select(getPattern).ToList();
                 //Console.WriteLine("Generated patterns");
 
-                //void RunPhase(int[] data, int[] output)
-                //{
-                //    var length = data.Length;
-                //    for (var ix = 0; ix < length; ix++)
-                //    {
-                //        int r = 0;
-                //        for (var jx = 0; jx < length; jx++)
-                //        {
-                //            switch (((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4)
-                //            {
-                //                case 1:
-                //                    r += data[jx];
-                //                    break;
-                //                case 3:
-                //                    r -= data[jx];
-                //                    break;
-                //            }
-                //        }
+                void RunPhase(int[] data, int[] output)
+                {
+                    var length = data.Length;
+                    Enumerable.Range(0, length).AsParallel().ForAll(ix =>
+                    {
+                        int r = 0;
+                        for (var jx = ix; jx < length; jx++)
+                        {
+                            r += data[jx];
+                            //switch (((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4)
+                            //{
+                            //    case 1:
+                            //        r += data[jx];
+                            //        break;
+                            //    case 3:
+                            //        r -= data[jx];
+                            //        break;
+                            //}
+                        }
 
-                //        output[ix] = r % 10;
-                //    }
-                //    //return data.Select((i, ix) =>
-                //    //{
-                //    //    //var pattern = patterns[ix];
-                //    //    return Math.Abs(
-                //    //               data.Select((j, jx) =>
-                //    //               {
-                //    //                   //var pattern = basePattern[((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4];
-                //    //                   //return (j * pattern);
-                //    //                   switch (((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4)
-                //    //                   {
-                //    //                       case 0:
-                //    //                       case 2:
-                //    //                           return 0;
-                //    //                       case 1:
-                //    //                           return j;
-                //    //                       case 3:
-                //    //                           return -j;
-                //    //                   }
+                        output[ix] = r % 10;
+                    });
+                    //for (var ix = 0; ix < length; ix++)
+                    //{
+                    //    int r = 0;
+                    //    for (var jx = ix; jx < length; jx++)
+                    //    {
+                    //        r += data[jx];
+                    //        //switch (((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4)
+                    //        //{
+                    //        //    case 1:
+                    //        //        r += data[jx];
+                    //        //        break;
+                    //        //    case 3:
+                    //        //        r -= data[jx];
+                    //        //        break;
+                    //        //}
+                    //    }
 
-                //    //                   throw new NotImplementedException();
-                //    //               }).Sum()) % 10;
-                //    //}).ToList();
-                //}
+                    //    output[ix] = r % 10;
+                    //}
+                    //return data.Select((i, ix) =>
+                    //{
+                    //    //var pattern = patterns[ix];
+                    //    return Math.Abs(
+                    //               data.Select((j, jx) =>
+                    //               {
+                    //                   //var pattern = basePattern[((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4];
+                    //                   //return (j * pattern);
+                    //                   switch (((jx + 1 + startPoint) / (ix + 1 + startPoint)) % 4)
+                    //                   {
+                    //                       case 0:
+                    //                       case 2:
+                    //                           return 0;
+                    //                       case 1:
+                    //                           return j;
+                    //                       case 3:
+                    //                           return -j;
+                    //                   }
+
+                    //                   throw new NotImplementedException();
+                    //               }).Sum()) % 10;
+                    //}).ToList();
+                }
 
                 // 32 = 320k * 320k = 102B
+                // 16k * 16k = 250M * 100
                 // 650 = 6.5M * 6.5M... 42T
+                // 500k * 500k = 2.5B * 100...
 
                 //for (var i = 0; i < numPhases; i++)
                 //{
@@ -156,13 +178,14 @@ namespace AoC.Year2019.Day16
                 //    numbers = RunPhase(numbers);
                 //}
 
-                //for (var i = 0; i < numPhases; i++)
-                //{
-                //    Console.WriteLine($"Running phase {i}");
-                //    var output = numbers.ToArray();
-                //    RunPhase(numbers, output);
-                //    numbers = output;
-                //}
+                for (var i = 0; i < numPhases; i++)
+                {
+                    Console.WriteLine($"Running phase {i}");
+                    //Console.WriteLine($"really running phase {i}");
+                    var output = numbers.ToArray();
+                    RunPhase(numbers, output);
+                    numbers = output;
+                }
 
 
                 //var lastPhase = numbers.ToArray();
@@ -170,7 +193,7 @@ namespace AoC.Year2019.Day16
                 //{
                 //    RunPhase(lastPhase, numbers);
                 //    Console.WriteLine($"Running phase {i}");
-                //    numbers = RunPhase(numbers);
+                //    //numbers = RunPhase(numbers);
                 //}
 
                 //Console.WriteLine(string.Join(",", getPattern(0)));
@@ -178,9 +201,9 @@ namespace AoC.Year2019.Day16
                 //Console.WriteLine(string.Join(",", getPattern(2)));
                 //Console.WriteLine(string.Join(",", getPattern(3)));
 
-                //Console.WriteLine(string.Join("", numbers.Skip(offset - startPoint).Take(8)));
+                Console.WriteLine(string.Join("", numbers.Skip(offset - startPoint).Take(8)));
 
-                Console.WriteLine(string.Join("", Enumerable.Range(offset, 8).Select(i => GetValue(100, i))));
+                //Console.WriteLine(string.Join("", Enumerable.Range(offset, 8).Select(i => GetValue(100, i))));
             });
         }
 

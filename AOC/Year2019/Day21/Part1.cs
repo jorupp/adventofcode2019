7ec4b140afcfc65d3bd5392167a36ad503b71d3d
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-20 21:35:02 -0600

    day 21 prep

---
+++ AOC/Year2019/Day21/Part1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AoC.Year2019.Day21
+{
+    public class Part1 : BasePart
+    {
+        protected void RunScenario(string title, string input)
+        {
+            RunScenario(title, () =>
+            {
+                var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Console.WriteLine(lines.Length);
+            });
+        }
+
+        public override void Run()
+        {
+            RunScenario("initial", @"asdfasdf");
+            //return;
+            RunScenario("part1", @"ff2f323f");
+
+        }
+    }
+}

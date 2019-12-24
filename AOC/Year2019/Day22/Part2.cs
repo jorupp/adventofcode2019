3181f33e91d5774f88e5fe9169b91965f1627224
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-23 19:29:17 -0600

    working !!!

--- AOC/Year2019/Day22/Part2.cs
+++ AOC/Year2019/Day22/Part2.cs
@@ -38,13 +38,18 @@ namespace AoC.Year2019.Day22
                 return new ModMathEvaluator((A * wrap.A).ModAbs(C), (wrap.A * B + wrap.B).ModAbs(C), C);
             }
 
+            public override string ToString()
+            {
+                return $"({A} * x + {B}) mod {C}";
+            }
+
             public BigInteger Apply(BigInteger input)
             {
                 return (A * input + B).ModAbs(C);
             }
         }
 
-        protected void RunScenario(string title, string input, long numCards, long targetPosition)
+        protected void RunScenario(string title, string input, long numCards, BigInteger targetPosition, long numShuffles)
         {
             RunScenario(title, () =>
             {
@@ -162,7 +167,29 @@ namespace AoC.Year2019.Day22
                 }
 
                 var compute = Compute();
+                Console.WriteLine($"compute: {compute}");
                 Console.WriteLine(compute.Apply(targetPosition));
+
+                long shufflesComputeIs = 1;
+                while (numShuffles > 0)
+                {
+                    Console.WriteLine($"Need to shuffle {numShuffles} more times");
+                    Console.WriteLine($"Formula to shuffle {shufflesComputeIs} times is {compute}");
+                    if (numShuffles % (shufflesComputeIs * 2) != 0)
+                    {
+                        Console.WriteLine($"Shuffling {shufflesComputeIs} times");
+                        targetPosition = compute.Apply(targetPosition);
+
+                        numShuffles -= shufflesComputeIs;
+                    }
+
+                    compute = compute.AsInputTo(compute);
+                    shufflesComputeIs *= 2;
+                }
+
+                Console.WriteLine(targetPosition);
+
+
                 //var valForZero = Compute()(0);
 
                 //// magic number derived from excel magic
@@ -399,107 +426,107 @@ namespace AoC.Year2019.Day22
 //            return;
 
 
-            RunScenario("part1", @"cut -1353
-deal with increment 63
-cut -716
-deal with increment 55
-cut 1364
-deal with increment 61
-cut 1723
-deal into new stack
-deal with increment 51
-cut 11
-deal with increment 65
-cut -6297
-deal with increment 69
-cut -3560
-deal with increment 20
-cut 1177
-deal with increment 29
-cut 6033
-deal with increment 3
-cut -3564
-deal into new stack
-cut 6447
-deal into new stack
-cut -4030
-deal with increment 3
-cut -6511
-deal with increment 42
-cut -8748
-deal with increment 38
-cut 5816
-deal with increment 73
-cut 9892
-deal with increment 16
-cut -9815
-deal with increment 10
-cut 673
-deal with increment 12
-cut 4518
-deal with increment 52
-cut 9464
-deal with increment 68
-cut 902
-deal with increment 11
-deal into new stack
-deal with increment 45
-cut -5167
-deal with increment 68
-deal into new stack
-deal with increment 24
-cut -8945
-deal into new stack
-deal with increment 36
-cut 3195
-deal with increment 52
-cut -1494
-deal with increment 11
-cut -9658
-deal into new stack
-cut -4689
-deal with increment 34
-cut -9697
-deal with increment 39
-cut -6857
-deal with increment 19
-cut -6790
-deal with increment 59
-deal into new stack
-deal with increment 52
-cut -9354
-deal with increment 71
-cut 8815
-deal with increment 2
-cut 6618
-deal with increment 47
-cut -6746
-deal into new stack
-cut 1336
-deal with increment 53
-cut 6655
-deal with increment 17
-cut 8941
-deal with increment 25
-cut -3046
-deal with increment 14
-cut -7818
-deal with increment 25
-cut 4140
-deal with increment 60
-cut 6459
-deal with increment 27
-cut -6791
-deal into new stack
-cut 3821
-deal with increment 13
-cut 3157
-deal with increment 13
-cut 8524
-deal into new stack
-deal with increment 12
-cut 5944", 10007, 3589);
-            return;
+//            RunScenario("part1", @"cut -1353
+//deal with increment 63
+//cut -716
+//deal with increment 55
+//cut 1364
+//deal with increment 61
+//cut 1723
+//deal into new stack
+//deal with increment 51
+//cut 11
+//deal with increment 65
+//cut -6297
+//deal with increment 69
+//cut -3560
+//deal with increment 20
+//cut 1177
+//deal with increment 29
+//cut 6033
+//deal with increment 3
+//cut -3564
+//deal into new stack
+//cut 6447
+//deal into new stack
+//cut -4030
+//deal with increment 3
+//cut -6511
+//deal with increment 42
+//cut -8748
+//deal with increment 38
+//cut 5816
+//deal with increment 73
+//cut 9892
+//deal with increment 16
+//cut -9815
+//deal with increment 10
+//cut 673
+//deal with increment 12
+//cut 4518
+//deal with increment 52
+//cut 9464
+//deal with increment 68
+//cut 902
+//deal with increment 11
+//deal into new stack
+//deal with increment 45
+//cut -5167
+//deal with increment 68
+//deal into new stack
+//deal with increment 24
+//cut -8945
+//deal into new stack
+//deal with increment 36
+//cut 3195
+//deal with increment 52
+//cut -1494
+//deal with increment 11
+//cut -9658
+//deal into new stack
+//cut -4689
+//deal with increment 34
+//cut -9697
+//deal with increment 39
+//cut -6857
+//deal with increment 19
+//cut -6790
+//deal with increment 59
+//deal into new stack
+//deal with increment 52
+//cut -9354
+//deal with increment 71
+//cut 8815
+//deal with increment 2
+//cut 6618
+//deal with increment 47
+//cut -6746
+//deal into new stack
+//cut 1336
+//deal with increment 53
+//cut 6655
+//deal with increment 17
+//cut 8941
+//deal with increment 25
+//cut -3046
+//deal with increment 14
+//cut -7818
+//deal with increment 25
+//cut 4140
+//deal with increment 60
+//cut 6459
+//deal with increment 27
+//cut -6791
+//deal into new stack
+//cut 3821
+//deal with increment 13
+//cut 3157
+//deal with increment 13
+//cut 8524
+//deal into new stack
+//deal with increment 12
+//cut 5944", 10007, 3589);
+//            return;
             //return;
             //return;
             RunScenario("part1", @"cut -1353
@@ -601,7 +628,7 @@ deal with increment 13
 cut 8524
 deal into new stack
 deal with increment 12
-cut 5944", 119315717514047, 2020);
+cut 5944", 119315717514047, 2020, 101741582076661);
 
         }
     }

c960369ae7947589a364b36831cf4917ed7066c8
Jonathan Rupp <jrupp@rightpoint.com>
2019-12-24 22:57:12 -0600

    day 25 prep

--- AOC/Year2019/Day25/Part1.cs
+++ AOC/Year2019/Day25/Part1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using AOC.Year2019.Day15;
 
 namespace AoC.Year2019.Day25
 {
@@ -10,6 +12,19 @@ namespace AoC.Year2019.Day25
             RunScenario(title, () =>
             {
                 var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                //var initialData = lines[0].Split(',').Select(long.Parse).ToArray();
+                //var data = initialData.Select((i, ii) => new { i, ii }).ToDictionary(i => i.ii, i => i.i);
+                //var start = IntCodeSimulationState.Start(data);
+
+                //var state = new Dictionary<(int, int), bool>().WithDefault(false);
+
+                //for (var y = 0; y < lines.Length; y++)
+                //{
+                //    for (var x = 0; x < lines[0].Length; x++)
+                //    {
+                //        state[(x, y)] = lines[y][x] == '#';
+                //    }
+                //}
 
                 Console.WriteLine(lines.Length);
             });

773b86ddf845b0ac1d23e403e6764498bfb9774a
Jonathan Rupp <jrupp@rightpoint.com>
2020-12-03 23:30:55 -0600

    prep for day 5

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -17,7 +17,10 @@ namespace AoC
             //new Year2020.Day3.Part2().Run();
 
             //new Year2020.Day4.Part1().Run();
-            new Year2020.Day4.Part2().Run();
+            //new Year2020.Day4.Part2().Run();
+
+            new Year2020.Day5.Part1().Run();
+            //new Year2020.Day5.Part2().Run();
 
             if (Debugger.IsAttached)
             {

b68af3a16fe1bb9000d7aa94b6d3e3aaba633f61
Jonathan Rupp <jrupp@rightpoint.com>
2020-12-12 23:53:32 -0600

    day 13 solution

--- AOC/Year2020/Day13/Part2.cs
+++ AOC/Year2020/Day13/Part2.cs
@@ -5,72 +5,90 @@ namespace AoC.Year2020.Day13
 {
     public class Part2 : BasePart
     {
-        protected void RunScenario(string title, string input, long guess)
+        protected void RunScenario(string title, string input)
         {
             RunScenario(title, () =>
             {
                 var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
                 var busses = lines[0].Split(',').Select(i => i == "x" ? (long?)null : long.Parse(i));
 
-                var x = busses.Select((i, ix) =>
-                {
-                    if (i == null) return null; 
-                    return $"t mod {i} = {(i - ix + i) % i}";
-                }).ToList();
-
-                foreach (var xx in x)
-                {
-                    if (null == xx)
-                        continue;
-                    Console.WriteLine(xx);
-                }
-
-                var formulas = busses.Select((i, ix) =>
+                var withFormulas = busses.Select((i, ix) =>
                 {
                     if (i == null)
                         return null;
-                    return (Func<long, bool>) (t => t % i == (i - ix + i) % i);
+                    var v = i.Value;
+                    
+                    // t % v is the position of our bus at time t
+                    // we need this bus to arrive ix minutes later than t, so (t + ix) % v will be 0
+                    Console.WriteLine($"(t + {ix}) mod {v} = 0");
+                    return new { increment = v, formula = (Func<long, bool>) (t => (t + ix) % v == 0)};
                 }).Where(i => i != null).ToList();
 
-                var rnd = new Random();
+                // we solve this problem incrementally.
+                // if we only had bus 0, time 0 would solve the problem.
+                // since that obviously won't work, we need a higher number, but whatever it is,
+                //    it must be value + N * lcm
+                // where lcm is the least common multiple of the bus numbers so far
+                long value = 0;
+                long lcm = withFormulas[0].increment;
 
-                long min = guess / 2;
-                long max = guess * 2;
-                long mod = max - min;
-                long baseMod = busses.First().Value;
-
-                long tries = 0;
-                while (true)
+                foreach (var withFormula in withFormulas.Skip(1))
                 {
-                    tries++;
+                    Console.WriteLine($"LCM: {lcm}");
 
-                    var g = (rnd.Next() * rnd.Next()) % mod + min;
-                    if (formulas.All(i => i(g)))
+                    // all the busses we've solved up to this point will work with a solution of "value"
+                    // but they will also work for any value + N * LCM, where N can be any integer
+                    // so we repeatedly increment value by LCM looking for a value that works for the formula for this new bus
+                    while (!withFormula.formula(value))
                     {
-                        Console.WriteLine(g);
-                        return;
+                        value += lcm;
                     }
 
-                    if (tries % 1000000 == 0)
-                    {
-                        Console.WriteLine($"  {tries} tries");
-                    }
+                    // value works for this bus.  to make sure our next solutions work for this bus, we need to merge this bus's increment with the LCM we have already
+                    // ie. if we solve for bus 25, then solve for bus 15, when we solve for the next bus, we need to increment by LCM(25,15)
+                    //   if we incremented by 25*15 will miss some viable options
+                    lcm = determineLCM(withFormula.increment, lcm);
                 }
 
+                Console.WriteLine(value);
 
             });
         }
 
+        public static long determineLCM(long a, long b)
+        {
+            long num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (long i = 1; i < num2; i++)
+            {
+                long mult = num1 * i;
+                if (mult % num2 == 0)
+                {
+                    return mult;
+                }
+            }
+            return num1 * num2;
+        }
+
+
         public override void Run()
         {
-            RunScenario("initial", @"7,13,x,x,59,x,31,19", 1068788);
-            RunScenario("initial", @"17,x,13,19", 3500);
-            RunScenario("initial", @"67,7,59,61", 700000);
-            RunScenario("initial", @"67,x,7,59,61", 700000);
-            RunScenario("initial", @"67,7,x,59,61", 1200000);
-            RunScenario("initial", @"1789,37,47,1889", 1200000000);
+            RunScenario("initial", @"7,13,x,x,59,x,31,19");
+            RunScenario("initial", @"17,x,13,19");
+            RunScenario("initial", @"67,7,59,61");
+            RunScenario("initial", @"67,x,7,59,61");
+            RunScenario("initial", @"67,7,x,59,61");
+            RunScenario("initial", @"1789,37,47,1889");
             //return;
-            RunScenario("part1", @"23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,383,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,503,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37", 150000000000000);
+            RunScenario("part1", @"23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,383,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,503,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37");
 
         }
     }

8fe210a69489874b3f857938d64d2de16629d297
Jonathan Rupp <jrupp@rightpoint.com>
2020-12-14 23:22:04 -0600

    day 15

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -49,8 +49,8 @@ namespace AoC
             //new Year2020.Day14.Part1().Run();
             //new Year2020.Day14.Part2().Run();
 
-            new Year2020.Day15.Part1().Run();
-            //new Year2020.Day15.Part2().Run();
+            //new Year2020.Day15.Part1().Run();
+            new Year2020.Day15.Part2().Run();
 
             if (Debugger.IsAttached)
             {
--- AOC/Year2020/Day15/Part2.cs
+++ AOC/Year2020/Day15/Part2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AoC.Year2020.Day15
@@ -9,17 +10,55 @@ namespace AoC.Year2020.Day15
         {
             RunScenario(title, () =>
             {
-                var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                var lines = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+                var memory = new Dictionary<long, long>();
 
-                Console.WriteLine(lines.Length);
+                long last = 0;
+                for (var i = 0; i < 30000000; i++)
+                {
+                    long newOne = 0;
+                    if (i < lines.Count)
+                    {
+                        newOne = lines[i];
+                    }
+                    else
+                    {
+                        if (memory.TryGetValue(last, out var v))
+                        {
+                            //Console.WriteLine($"have seen before: {i} - {v}");
+                            newOne = i - v;
+                        }
+                        else
+                        {
+                            newOne = 0;
+                        }
+                    }
+
+                    //Console.WriteLine($"Turn {i}: said {newOne}");
+                    if (i > 0)
+                    {
+                        //Console.WriteLine($"Recorded {last} as {i}");
+                        memory[last] = i;
+                    }
+                    last = newOne;
+                }
+
+                Console.WriteLine(last);
             });
         }
 
         public override void Run()
         {
-            RunScenario("initial", @"asdfasdf");
+            RunScenario("initial", @"0,3,6");
+            RunScenario("initial", @"1,3,2");
             //return;
-            RunScenario("part1", @"ff2f323f");
+            RunScenario("initial", @"2,1,3");
+            RunScenario("initial", @"1,2,3");
+            RunScenario("initial", @"2,3,1");
+            RunScenario("initial", @"3,2,1");
+            RunScenario("initial", @"3,1,2");
+            //return;
+            RunScenario("part1", @"14,1,17,0,3,20");
 
         }
     }

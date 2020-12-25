1d463c197de49ea44d4b9fe26bab3f99b284fa77
Jonathan Rupp <jrupp@rightpoint.com>
2020-12-24 23:22:17 -0600

    day 25

--- AOC/Year2020/Day25/Part1.cs
+++ AOC/Year2020/Day25/Part1.cs
@@ -5,22 +5,57 @@ namespace AoC.Year2020.Day25
 {
     public class Part1 : BasePart
     {
-        protected void RunScenario(string title, string input)
+        protected void RunScenario(string title, int cardPublicKey, int doorPublicKey)
         {
             RunScenario(title, () =>
             {
-                var lines = input.Replace("\r\n", "\n").Split("\n", StringSplitOptions.RemoveEmptyEntries);
+                var cardLoopSize = FindLoopSize(7, cardPublicKey);
+                var doorLoopSize = FindLoopSize(7, doorPublicKey);
 
-                Console.WriteLine(lines.Length);
+                Console.WriteLine($"cardLoopSize: {cardLoopSize}");
+                Console.WriteLine($"doorLoopSize: {doorLoopSize}");
+
+                var key1 = Transform(cardPublicKey, doorLoopSize);
+                var key2 = Transform(doorPublicKey, cardLoopSize);
+
+                Console.WriteLine($"key1: {key1}");
+                Console.WriteLine($"key2: {key2}");
+
             });
+        }
+
+        // 12495346 is wrong
+
+        long Transform(long subjectNumber, long loopSize)
+        {
+            long value = 1;
+            for(long i=0; i<loopSize; i++)
+            {
+                value = value * subjectNumber % 20201227;
+            }
+
+            return value;
+        }
+
+        long FindLoopSize(long subjectNumber, long targetValue)
+        {
+            long value = 1;
+            var loopSize = 0;
+            while(value != targetValue)
+            {
+                // a*x + b mod c
+                value = value * subjectNumber % 20201227;
+                loopSize++;
+            }
+
+            return loopSize;
         }
 
         public override void Run()
         {
-            RunScenario("initial", @"asdfasdf");
+            RunScenario("initial", 5764801, 17807724);
             //return;
-            RunScenario("part1", @"ff2f323f");
-
+            RunScenario("part1", 18499292, 8790390);
         }
     }
 }

5d0341d31adecdd4a6ceda94c3bdd09e0fdcea37
Jonathan Rupp <jrupp@rightpoint.com>
2022-12-14 20:21:38 -0600

    Create plateholder for 2022 and exclude old code from the project (but not the repo)

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,86 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace AoC
+namespace AOC
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //new Year2020.Day1.Part1().Run();
-            //new Year2020.Day1.Part2().Run();
-
-            //new Year2020.Day2.Part1().Run();
-            //new Year2020.Day2.Part2().Run();
-
-            //new Year2020.Day3.Part1().Run();
-            //new Year2020.Day3.Part2().Run();
-
-            //new Year2020.Day4.Part1().Run();
-            //new Year2020.Day4.Part2().Run();
-
-            //new Year2020.Day5.Part1().Run();
-            //new Year2020.Day5.Part2().Run();
-
-            //new Year2020.Day6.Part1().Run();
-            //new Year2020.Day6.Part2().Run();
-
-            //new Year2020.Day7.Part1().Run();
-            //new Year2020.Day7.Part2().Run();
-
-            //new Year2020.Day8.Part1().Run();
-            //new Year2020.Day8.Part2().Run();
-
-            //new Year2020.Day9.Part1().Run();
-            //new Year2020.Day9.Part2().Run();
-
-            //new Year2020.Day10.Part1().Run();
-            //new Year2020.Day10.Part2().Run();
-
-            //new Year2020.Day11.Part1().Run();
-            //new Year2020.Day11.Part2().Run();
-
-            //new Year2020.Day12.Part1().Run();
-            //new Year2020.Day12.Part2().Run();
-
-            //new Year2020.Day13.Part1().Run();
-            //new Year2020.Day13.Part2().Run();
-
-            //new Year2020.Day14.Part1().Run();
-            //new Year2020.Day14.Part2().Run();
-
-            //new Year2020.Day15.Part1().Run();
-            //new Year2020.Day15.Part2().Run();
-
-            //new Year2020.Day16.Part1().Run();
-            //new Year2020.Day16.Part2().Run();
-
-            //new Year2020.Day17.Part1().Run();
-            //new Year2020.Day17.Part2().Run();
-
-            //new Year2020.Day18.Part1().Run();
-            //new Year2020.Day18.Part2().Run();
-            //new Year2020.Day18.Part2b().Run();
-
-            //new Year2020.Day19.Part1().Run();
-            //new Year2020.Day19.Part2().Run();
-
-            //new Year2020.Day20.Part1().Run();
-            //new Year2020.Day20.Part2().Run();
-
-            //new Year2020.Day21.Part1().Run();
-
-            //new Year2020.Day22.Part1().Run();
-            //new Year2020.Day22.Part2().Run();
-
-            //new Year2020.Day23.Part1().Run();
-            //new Year2020.Day23.Part2().Run();
-
-            //new Year2020.Day24.Part1().Run();
-            //new Year2020.Day24.Part2().Run();
-
-            new Year2020.Day25.Part1().Run();
-            //new Year2020.Day25.Part2().Run();
+            new Year2022.Day0.Part1().Run();
+            new Year2022.Day0.Part2().Run();
 
             if (Debugger.IsAttached)
             {

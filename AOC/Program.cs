e688ae43ac7439e8ec24670b157af0864f9d1ac0
Jonathan Rupp <jorupp@gmail.com>
2022-12-14 23:00:47 -0600

    Prep for day 5

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -7,8 +7,8 @@ namespace AOC
     {
         static void Main(string[] args)
         {
-            new Year2022.Day4.Part1().Run();
-            new Year2022.Day4.Part2().Run();
+            new Year2022.Day5.Part1().Run();
+            new Year2022.Day5.Part2().Run();
 
             if (Debugger.IsAttached)
             {

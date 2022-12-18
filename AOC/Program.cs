74ef853c0a90503efab7bf0db67bab0acac7ad67
Jonathan Rupp <jorupp@gmail.com>
2022-12-18 12:22:45 -0600

    prep for day 13

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -7,8 +7,8 @@ namespace AOC
     {
         static void Main(string[] args)
         {
-            new Year2022.Day12.Part1().Run();
-            new Year2022.Day12.Part2().Run();
+            new Year2022.Day13.Part1().Run();
+            new Year2022.Day13.Part2().Run();
 
             if (Debugger.IsAttached)
             {
---
+++ AOC/Year2022/Day13/Part1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using AoC;
+
+namespace AOC.Year2022.Day13
+{
+    public class Part1 : BasePart
+    {
+        protected void RunScenario(string title, string input)
+        {
+            RunScenario(title, () =>
+            {
+                var lines = input.Replace("\r\n", "\n").Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+                Console.WriteLine(lines.Length);
+            });
+        }
+
+        public override void Run()
+        {
+            RunScenario("initial", @"asdfasdf");
+            //return;
+            RunScenario("part1", @"ff2f323f");
+
+        }
+    }
+}
